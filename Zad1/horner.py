1f6570a6a560f485f1ff2e5e8386c7ad07a39825
PrzemyslawMusial242473 <126701320+PrzemyslawMusial242473@users.noreply.github.com>
2023-03-20 20:15:28 +0100

    Update horner.py

--- Zad1/horner.py
+++ Zad1/horner.py
@@ -1,19 +1,5 @@
-def horner(lista_wspolczynnikow, x):
-    """Zwracanie wartości wybranego wielomianu z użyciem schematu Hornera
-
-            Parametry
-            ----------
-            lista_wspolczynnikow : int list
-                wartości współczynników wielomianu zapisane w liście
-                w kolejności od stojącego przy najwyższej potędze do wyrazu wolnego
-            x : float
-                argument dla którego obliczamy wartość funkcji
-            Dane wyjsciowe
-            -------
-            wynik : float
-                wynik obliczeń
-    """
+def horner(xlist, x):
     wynik = 0
-    for item in reversed(lista_wspolczynnikow):    # pobiera współczynniki wielomianu od końca
-        wynik = wynik * x + item
-    return wynik    # zwrot wartosci wielomianu
+    for xy in reversed(xlist):    
+        wynik = wynik * x + xy
+    return wynik  
