917c834b36f01a9d24ac825d5d70f31f2965506e
PrzemyslawMusial242473 <126701320+PrzemyslawMusial242473@users.noreply.github.com>
2023-03-28 22:15:34 +0200

    Update funkcje.py

--- Zad2/funkcje.py
+++ Zad2/funkcje.py
@@ -1,29 +1,51 @@
-
 import numpy as np
 
-def gauss_seidel(A, b, x0, epsilon, max_iterations):
-    n = len(A)
-    x = x0.copy()
-
-    for i in range(max_iterations):
-        x_new = np.zeros(n)
-        for j in range(n):
-            s1 = np.dot(A[j, :j], x_new[:j])
-            s2 = np.dot(A[j, j + 1:], x[j + 1:])
-            x_new[j] = (b[j] - s1 - s2) / A[j, j]
-        if np.allclose(x, x_new, rtol=epsilon):
-            return x_new
-        x = x_new
-    return x
-
-A = np.array([[0.5, -0.0625, 0.1875, 0.0625],
+def Seidl(eps,ITERATION_LIMIT):
+    A = np.array([[0.5, -0.0625, 0.1875, 0.0625],
               [-0.0625, 0.5, 0, 0],
               [0.1875, 0, 0.375, 0.125],
               [0.0625, 0, 0.125, 0.25]])
-b = np.array([1.5,-1.625, 1, 0.4375])
-x0 = np.zeros(4)
-eps = 0.000000001
-max_iter = 100
+    b = np.array([1.5,-1.625, 1, 0.4375])
 
-x = gauss_seidel(A, b, x0, eps, max_iter)
-print(x)
+    # prints the system
+    print("System:")
+    for i in range(A.shape[0]):
+        row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
+        print(" + ".join(row), "=", b[i])
+        print()
+
+    x = np.zeros_like(b)
+    if ITERATION_LIMIT==0:
+        r = np.zeros_like(x)
+        h = 0
+        while True:
+            print("Current solution:", x)
+            x_new = np.zeros_like(x)
+            for i in range(A.shape[0]):
+                s1 = np.dot(A[i, :i], x_new[:i])
+                s2 = np.dot(A[i, i + 1:], x[i + 1:])
+                x_new[i] = (b[i] - s1 - s2) / A[i, i]
+                r[i] = b[i] - s1
+            x=x_new   
+            h = h + 1
+            if h == 14:
+                print("Podano za mały epsilon")
+                break
+            elif abs(np.max(r)) < eps:
+                print("Znaleziono rozwiązanie po "+ str(h) +" iteracjach")  
+                break      
+    else:
+        for it_count in range(ITERATION_LIMIT):
+            print("Current solution:", x)
+            x_new = np.zeros_like(x)
+            for i in range(A.shape[0]):
+                s1 = np.dot(A[i, :i], x_new[:i])
+                s2 = np.dot(A[i, i + 1:], x[i + 1:])
+                x_new[i] = (b[i] - s1 - s2) / A[i, i]
+            if np.allclose(x, x_new, rtol=eps):
+                break
+            x = x_new
+
+    print("Solution:")
+    print(x)
+
